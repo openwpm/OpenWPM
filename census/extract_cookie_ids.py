fe18abc0c90c213465f89901095a32e64ca6bb9b
englehardt <englehardt@gmail.com>
2014-04-22 00:50:45 -0400

    added three-crawl extraction

--- census/extract_cookie_ids.py
+++ census/extract_cookie_ids.py
@@ -31,6 +31,7 @@ def extract_cookie_candidates_from_db(db_name):
         if census_util.all_same(raw_cookie_dict[cookie]):
             cookie_dict[cookie] = raw_cookie_dict[cookie][0]
 
+    con.close()
     return cookie_dict
 
 # goes through the cookies values and looks for values of the form id=XXX&time=YYYY
@@ -73,6 +74,40 @@ def extract_common_persistent_ids(cookie_dicts):
 
     return domain_dict
 
+# takes in three dictionaries of persistent, non-session cookies
+# two from a repeat crawl with the same database and one from a fresh crawl
+# with a fresh database. From this, ids are classified as follows:
+# between two separate profiles
+#   - values of the same length
+#   - are not the same and have pairwise similarities < 0.9 (which covers all being equal)
+#   - appears in both crawls (otherwise, can't make a definitive statement about it)
+#   - length >= 5 and <= 100
+# between two crawls with same profile:
+#   - values are the same
+# returns dictionary with domains as keys and cookie names as values
+# INPUT must be of the form: [profile_1_pass_1, profile_1_pass_2, profile_2]
+def extract_persistent_ids(cookie_dicts):
+    raw_id_dict = defaultdict(list)  # for each cookie, a list of the values across each crawl
+
+    # combine all smaller cookie dictionaries into a larger dictionary
+    for cookie_dict in cookie_dicts:
+        for cookie in cookie_dict:
+            raw_id_dict[cookie].append(cookie_dict[cookie])
+
+    domain_dict = defaultdict(list)  # for each domain,list of candidate ID cookies
+
+    # prune away cookies that fail one of our unique ID heuristics
+    for cookie in raw_id_dict:
+        if len(raw_id_dict[cookie]) <= 2 or len(raw_id_dict[cookie][0]) <= 5 or len(raw_id_dict[cookie][0]) > 100 \
+                or not raw_id_dict[cookie][0] == raw_id_dict[cookie][1] \
+                or not census_util.all_same_len(raw_id_dict[cookie][1:2]) \
+                or not census_util.all_dissimilar(raw_id_dict[cookie][1:2]):
+            continue
+
+        domain_dict[cookie[0]].append(cookie[1])
+
+    return domain_dict
+
 # given a dictionary of persistent ids, goes through a database
 # and returns a dictionary with the persistent ids and their unique values
 # in the database (for those that actually appear)
@@ -96,11 +131,37 @@ def extract_known_cookies_from_db(db_name, cookie_dict):
                         params = part.split("=")
                         if len(params) == 2 and name + "#" + params[0] in cookie_dict[domain]:
                             found_cookies[(domain, name + "#" + params[0])] = params[1]
+    con.close()
+    return found_cookies
 
+# given a dictionary of persistent ids, goes through a database
+# and returns a dictionary with the persistent ids and their unique values
+# in the database, but only returns the first seen cookie for each ID (the assumed owner)
+def extract_first_seen_from_db(db_name, cookie_dict):
+    con = lite.connect(db_name)
+    cur = con.cursor()
+
+    found_cookies = {}
+    for domain, name, value, accessed in cur.execute('SELECT domain, name, value, accessed FROM cookies'):
+        domain = domain if len(domain) == 0 or domain[0] != "." else domain[1:]
+
+        # first search for most basic cookies
+        if domain in cookie_dict and name in cookie_dict[domain]:
+            found_cookies[(domain, name)] = value
+
+            # next, look for potential nested cookies
+            if "=" in value:
+                for delimiter in ["&", ":"]:
+                    parts = value.split(delimiter)
+                    for part in parts:
+                        params = part.split("=")
+                        if len(params) == 2 and name + "#" + params[0] in cookie_dict[domain]:
+                            found_cookies[(domain, name + "#" + params[0])] = params[1]
+    con.close()
     return found_cookies
 
 if __name__ == "__main__":
     c1 = extract_cookie_candidates_from_db("/home/christian/Desktop/crawl1.sqlite")
     c2 = extract_cookie_candidates_from_db("/home/christian/Desktop/crawl2.sqlite")
-    extracted = extract_common_persistent_ids([c1, c2])
+    extracted = extract_persistent_ids([c1, c2])
     known = extract_known_cookies_from_db("/home/christian/Desktop/crawl1.sqlite", extracted)
