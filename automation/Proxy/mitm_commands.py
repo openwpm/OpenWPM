ae4ecd4b9dbbac48238887f84585139f164a40ff
Christian <christian.eubank@gmail.com>
2014-05-01 23:40:03 -0400

    Log request cookies and additional responses

--- automation/Proxy/mitm_commands.py
+++ automation/Proxy/mitm_commands.py
@@ -1,11 +1,9 @@
 # This module parses MITM Proxy requests/reponses into (command, data pairs)
 # This should mean that the MITMProxy code should simply pass the messages + its own data to this module
-# TODO: deal with JavaScript and Cookies
+# TODO: deal with JavaScript
 
 import datetime
 from dateutil import parser
-import base64
-import zlib
 
 # msg is the message object given by MITM
 # (crawl_id, url, method, referrer, top_url)
@@ -14,6 +12,10 @@ def process_general_mitm_request(db_socket, crawl_id, top_url, msg):
         referrer = msg.headers['referer'][0]
     else:
         referrer = ''
+
+    # log cookies if they exist
+    if msg.get_cookies() is not None:
+        process_cookies(db_socket, crawl_id, top_url, referrer, msg.get_cookies(), "request")
 
     data = (crawl_id, msg.get_url(), msg.method, referrer, str(msg.headers), top_url, str(datetime.datetime.now()))
     db_socket.send(("INSERT INTO http_requests (crawl_id, url, method, referrer, headers, top_url, time_stamp) VALUES (?,?,?,?,?,?,?)", data))
@@ -32,14 +34,12 @@ def process_general_mitm_response(db_socket, crawl_id, top_url, msg):
     else:
         location = ''
 
-    # currently, either log request as a cookie or standard request
-    # in the future, may want to add JavaScript table
+    # log cookies if they exist
     if msg.get_cookies() is not None:
-        process_cookies(db_socket, crawl_id, top_url, referrer, msg.get_cookies())
+        process_cookies(db_socket, crawl_id, top_url, referrer, msg.get_cookies(), "response")
 
-    else:
-        data = (crawl_id, msg.request.get_url(), msg.request.method, referrer, msg.code, msg.msg, str(msg.headers), location, top_url, str(datetime.datetime.now()))
-        db_socket.send(("INSERT INTO http_responses (crawl_id, url, method, referrer, response_status, "
+    data = (crawl_id, msg.request.get_url(), msg.request.method, referrer, msg.code, msg.msg, str(msg.headers), location, top_url, str(datetime.datetime.now()))
+    db_socket.send(("INSERT INTO http_responses (crawl_id, url, method, referrer, response_status, "
                       "response_status_text, headers, location, top_url, time_stamp) VALUES (?,?,?,?,?,?,?,?,?,?)", data))
 
 # returns canonical date-time string
@@ -50,7 +50,7 @@ def parse_date(date):
         return str(datetime.datetime.now())
 
 # add an entry for a cookie to the table
-def process_cookies(db_socket, crawl_id, top_url, referrer, cookies):
+def process_cookies(db_socket, crawl_id, top_url, referrer, cookies, http_type):
     for name in cookies:
         value, attr_dict = cookies[name]
         domain = '' if 'domain' not in attr_dict else unicode(attr_dict['domain'], errors='ignore')
@@ -58,6 +58,6 @@ def process_cookies(db_socket, crawl_id, top_url, referrer, cookies):
         expiry = str(datetime.datetime.now()) if 'expires' not in attr_dict else parse_date(attr_dict['expires'])
 
         data = (crawl_id, domain, unicode(name, errors='ignore'), unicode(value, errors='ignore'),
-                expiry, accessed, referrer, top_url)
-        db_socket.send(("INSERT INTO cookies (crawl_id, domain, name, value, expiry, accessed, referrer, top_url) "
-                      "VALUES (?,?,?,?,?,?,?,?)", data))
+                expiry, accessed, referrer, http_type, top_url)
+        db_socket.send(("INSERT INTO cookies (crawl_id, domain, name, value, expiry, accessed, referrer, http_type, top_url) "
+                      "VALUES (?,?,?,?,?,?,?,?,?)", data))
