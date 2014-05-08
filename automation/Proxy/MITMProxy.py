86ce53a2413c2615c1ae68031d6999b9aa14e31e
Christian <christian.eubank@gmail.com>
2014-05-08 13:20:36 -0400

    Undoing merge from class project

--- automation/Proxy/MITMProxy.py
+++ automation/Proxy/MITMProxy.py
@@ -11,8 +11,7 @@ from tld import get_tld
 # Inspired by the following example. Note the gist has a lot of bugs.
 # https://gist.github.com/dannvix/5285924
 class InterceptingMaster (controller.Master):
-    def __init__(self, server, crawl_id, url_queue, db_socket_address, url_filter):
-        self.url_filter = url_filter
+    def __init__(self, server, crawl_id, url_queue, db_socket_address):
         self.crawl_id = crawl_id
         
         # Attributes used to flag the first-party domain
--- automation/Proxy/deploy_mitm_proxy.py
+++ automation/Proxy/deploy_mitm_proxy.py
@@ -4,7 +4,6 @@ import socket
 import thread
 
 import MITMProxy
-from URLFilter import URLFilter
 from libmproxy import proxy
 
 # deploys an instance of mitmproxy
@@ -19,14 +18,10 @@ def init_proxy(db_socket_address, crawl_id):
     sock.bind(('', 0))
     proxy_port = sock.getsockname()[1]
     sock.close()
- 
-    # load ad_nets.list to the URLFilter
-    url_filt = URLFilter.from_file("ad_nets.list")
 
     config = proxy.ProxyConfig(cacert=os.path.join(os.path.dirname(__file__), 'mitmproxy.pem'),)
     server = proxy.ProxyServer(config, proxy_port)
     print 'Intercepting Proxy listening on ' + str(proxy_port)
-    m = MITMProxy.InterceptingMaster(server, crawl_id, proxy_site_queue, db_socket_address, url_filt)
+    m = MITMProxy.InterceptingMaster(server, crawl_id, proxy_site_queue, db_socket_address)
     thread.start_new_thread(m.run, ())
-
     return proxy_port, proxy_site_queue
