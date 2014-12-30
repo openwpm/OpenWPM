bf85a7797dbad3eec7e33a24cd5d8273893f30cf
englehardt <englehardt@gmail.com>
2014-12-30 18:48:09 -0500

    referer now working in HTTP response

--- automation/Proxy/mitm_commands.py
+++ automation/Proxy/mitm_commands.py
@@ -6,6 +6,7 @@ import datetime
 def process_general_mitm_request(db_socket, crawl_id, top_url, msg):
     """ Logs a HTTP request object """
     referrer = msg.request.headers['referer'][0] if len(msg.request.headers['referer']) > 0 else ''
+    
     data = (crawl_id, msg.request.url, msg.request.method, referrer, str(msg.request.headers), top_url, str(datetime.datetime.now()))
     db_socket.send(("INSERT INTO http_requests (crawl_id, url, method, referrer, headers, "
                     "top_url, time_stamp) VALUES (?,?,?,?,?,?,?)", data))
@@ -13,8 +14,9 @@ def process_general_mitm_request(db_socket, crawl_id, top_url, msg):
 
 def process_general_mitm_response(db_socket, crawl_id, top_url, msg):
     """ Logs a HTTP response object and, if necessary, """
-    referrer = msg.response.headers['referer'][0] if len(msg.response.headers['referer']) > 0 else ''
+    referrer = msg.request.headers['referer'][0] if len(msg.request.headers['referer']) > 0 else ''
     location = msg.response.headers['location'][0] if len(msg.response.headers['location']) > 0 else ''
+    
     data = (crawl_id, msg.request.url, msg.request.method, referrer, msg.response.code, msg.response.msg, str(msg.response.headers),
             location, top_url, str(datetime.datetime.now()))
     db_socket.send(("INSERT INTO http_responses (crawl_id, url, method, referrer, response_status, "
