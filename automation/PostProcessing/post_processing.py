538d1cb277964a2d384c7016a04dde85bb5a16c7
englehardt <englehardt@gmail.com>
2015-06-05 16:06:09 -0400

    removing logging from post_processing, server is closed at that point

--- automation/PostProcessing/post_processing.py
+++ automation/PostProcessing/post_processing.py
@@ -1,9 +1,7 @@
 from build_cookie_table import build_http_cookie_table  
 
-import logging
-
-def run(db_path):
+def run(db_path, loggingaddress):
     """ Post-processing tasks to run after TaskManager finishes """
-    logging.info("Starting post-processing tasks...")
+    print "Starting post-processing tasks..."
     
     build_http_cookie_table(db_path) # Parse HTTP Cookies
