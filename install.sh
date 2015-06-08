3791d116053fa04fedf67e2a1a4c700d70a7fa83
englehardt <englehardt@gmail.com>
2015-06-08 21:32:29 +0000

    upgrade selenium to 2.46.0 and firefox to 38

--- install.sh
+++ install.sh
@@ -2,12 +2,17 @@ sudo apt-get update
 
 sudo apt-get install firefox htop git python-dev python-pip libxml2-dev libxslt-dev libffi-dev libssl-dev build-essential xvfb
 
-sudo pip install -U pyvirtualdisplay beautifulsoup4 netlib pyasn1 PyOPenSSL mitmproxy python-dateutil tld pyamf psutil
+sudo pip install -U pyvirtualdisplay beautifulsoup4 netlib pyasn1 PyOPenSSL python-dateutil tld pyamf psutil
 
-# Install selenium v2.44.0
-sudo pip install selenium==2.44.0
+# Pin a specific version of mitmproxy
+# current version has compatibility issues with 
+# the most recent cffi release
+sudo pip install mitmproxy==0.11.2
+sudo pip install cffi=1.0.3
 
-# Install firefox v35
-wget https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/35.0.1/linux-x86_64/en-US/firefox-35.0.1.tar.bz2
+# Install specific version of Firefox and selenium
+# known to work well together.
+sudo pip install selenium==2.46.0
+wget https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/38.0.5/linux-x86_64/en-US/firefox-38.0.5.tar.bz2
 tar jxf firefox*.tar.bz2 -C ./
 rm firefox*.tar.bz2
