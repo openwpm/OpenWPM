544031e3aa551b54a6361bef90964bc2fce43d4d
englehardt <englehardt@gmail.com>
2015-11-08 00:25:11 +0000

    Use newest version of pip, instead of the outdated version from Ubuntu's repository.

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 sudo apt-get update
 
-sudo apt-get install firefox htop git python-dev python-pip libxml2-dev libxslt-dev libffi-dev libssl-dev build-essential xvfb libboost-python-dev libleveldb1 libleveldb-dev libjpeg-dev
+sudo apt-get install firefox htop git python-dev libxml2-dev libxslt-dev libffi-dev libssl-dev build-essential xvfb libboost-python-dev libleveldb1 libleveldb-dev libjpeg-dev
 
-sudo pip install -U setuptools
-sudo pip install -U pyvirtualdisplay beautifulsoup4 netlib pyasn1 PyOPenSSL python-dateutil tld pyamf psutil mitmproxy pyhash plyvel tblib
+wget https://bootstrap.pypa.io/get-pip.py
+sudo -H python get-pip.py
+rm get-pip.py
+
+sudo -H pip install -U setuptools
+sudo -H pip install -U pyvirtualdisplay beautifulsoup4 netlib pyasn1 PyOPenSSL python-dateutil tld pyamf psutil mitmproxy pyhash plyvel tblib
 
 # Install specific version of Firefox and selenium
 # known to work well together.
