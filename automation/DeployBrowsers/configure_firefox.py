65a9bee7084ef38daf7ee3d8ea327f2f383268a0
englehardt <englehardt@gmail.com>
2016-01-20 18:42:16 -0800

    Updating to newest version of Ghostery

--- automation/DeployBrowsers/configure_firefox.py
+++ automation/DeployBrowsers/configure_firefox.py
@@ -36,7 +36,7 @@ def privacy(browser_params, fp, root_dir, browser_profile_path):
 
     # Load Ghostery - Enable all blocking
     if browser_params['ghostery']:
-        fp.add_extension(extension=os.path.join(root_dir,'firefox_extensions/ghostery/ghostery-5.4.6.xpi'))
+        fp.add_extension(extension=os.path.join(root_dir,'firefox_extensions/ghostery/ghostery-5.4.10.xpi'))
         os.makedirs(browser_profile_path+'jetpack/firefox@ghostery.com/simple-storage/')
         src = os.path.join(root_dir,'firefox_extensions/ghostery/store.json') # settings - block all trackers/cookies
         dst = os.path.join(browser_profile_path,'jetpack/firefox@ghostery.com/simple-storage/store.json')
