93e7677a9a8bd779617e192b1acaf5f0c7bcc510
englehardt <englehardt@gmail.com>
2016-05-03 11:54:23 -0400

    Minor changes to dev install script

--- install-dev.sh
+++ install-dev.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+set -e
+
 # Dependencies for OpenWPM development -- NOT needed to run the platform.
 # * Required for compiling Firefox extension
 sudo apt-get install npm
 
 # Fix naming issue
-ln -s /usr/bin/nodejs /usr/bin/node
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 sudo npm install jpm -g
