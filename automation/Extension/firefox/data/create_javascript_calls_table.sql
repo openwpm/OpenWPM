2ac4bed29405c05bfbe9da807336ee7e37a90ff9
Steven Englehardt <englehardt@gmail.com>
2016-05-05 21:47:14 -0400

    Replacing tabs with spaces

--- automation/Extension/firefox/data/create_javascript_calls_table.sql
+++ automation/Extension/firefox/data/create_javascript_calls_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS javascript_calls(
-	id INTEGER PRIMARY KEY ASC,
+    id INTEGER PRIMARY KEY ASC,
     crawl_id INTEGER,
-	visit_id INTEGER,
+    visit_id INTEGER,
     javascript_id INTEGER,
-	parameter_index INTEGER,
-	value TEXT
+    parameter_index INTEGER,
+    value TEXT
 );
