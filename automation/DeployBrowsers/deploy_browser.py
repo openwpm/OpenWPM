fe04301f0a4ec983bc9b7410be01afd99dea3f9d
englehardt <englehardt@gmail.com>
2016-12-10 11:49:50 -0500

    Removing unsupported Chrome deployment code and raising an error when `browser_params['browser'] = 'chrome'`.

--- automation/DeployBrowsers/deploy_browser.py
+++ automation/DeployBrowsers/deploy_browser.py
@@ -1,9 +1,10 @@
-import deploy_chrome
 import deploy_firefox
+from ..Errors import BrowserConfigError
 
 def deploy_browser(status_queue, browser_params, manager_params, crash_recovery):
     """ receives a dictionary of browser parameters and passes it to the relevant constructor """
     if browser_params['browser'].lower() == 'chrome':
-        return deploy_chrome.deploy_chrome(browser_params)
+        raise BrowserConfigError("Chrome is not supported. OpenWPM currently "
+                                 "only supports measurement with Firefox.")
     if browser_params['browser'].lower() == 'firefox':
         return deploy_firefox.deploy_firefox(status_queue, browser_params, manager_params, crash_recovery)
