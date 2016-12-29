4431c4c73a72b879c7ca12de40fded38f4847ca9
englehardt <englehardt@gmail.com>
2016-12-29 10:18:58 -0500

    Remove sql file for unused table

--- automation/Extension/firefox/data/create_javascript_calls_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS javascript_calls(
-    id INTEGER PRIMARY KEY ASC,
-    crawl_id INTEGER,
-    visit_id INTEGER,
-    javascript_id INTEGER,
-    parameter_index INTEGER,
-    value TEXT
-);
