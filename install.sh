2cf09180a4c88260a6510b6a4e7ceaccc30b5ea0
englehardt <englehardt@gmail.com>
2016-12-30 16:02:03 -0500

    Updating Firefox to newest ESR version of 45

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ if [ "$TRAVIS" != "true" ]; then
 fi
 
 # Install specific version of Firefox known to work well with the selenium version above
-wget https://ftp.mozilla.org/pub/firefox/releases/45.0.1/linux-x86_64/en-US/firefox-45.0.1.tar.bz2
+wget https://ftp.mozilla.org/pub/firefox/releases/45.6.0esr/linux-x86_64/en-US/firefox-45.6.0esr.tar.bz2
 tar jxf firefox*.tar.bz2
 rm -rf firefox-bin
 mv firefox firefox-bin
