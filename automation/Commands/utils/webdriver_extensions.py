0d9e88fbefcff95da93911cf697be8efc349350f
englehardt <englehardt@gmail.com>
2017-08-08 16:11:58 -0400

    Updating webdriver extensions to newest version

--- automation/Commands/utils/webdriver_extensions.py
+++ automation/Commands/utils/webdriver_extensions.py
@@ -3,21 +3,25 @@
 
 from __future__ import absolute_import
 
+from six.moves.urllib.parse import urljoin
 import random
 import time
-from six.moves.urllib.parse import urljoin
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import ElementNotVisibleException
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import StaleElementReferenceException
 
 from . import XPathUtil
 from ...utilities import domain_utils as du
 
 
 # Basic functions
+
 def scroll_down(driver):
     at_bottom = False
     while random.random() > .20 and not at_bottom:
@@ -30,8 +34,11 @@ def scroll_down(driver):
 
 
 def scroll_to_bottom(driver):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    return
+    try:
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+    except WebDriverException:
+        pass
 
 
 def is_loaded(webdriver):
@@ -50,11 +57,20 @@ def wait_until_loaded(webdriver, timeout, period=0.25):
 
 def get_intra_links(webdriver, url):
     ps1 = du.get_ps_plus_1(url)
-    return [
-        x for x in webdriver.find_elements_by_tag_name("a")
-        if (x.get_attribute("href") and
-            du.get_ps_plus_1(urljoin(url, x.get_attribute("href"))) == ps1)
-    ]
+    links = list()
+    for elem in webdriver.find_elements_by_tag_name("a"):
+        try:
+            href = elem.get_attribute('href')
+        except StaleElementReferenceException:
+            continue
+        if href is None:
+            continue
+        full_href = urljoin(url, href)
+        if not full_href.startswith('http'):
+            continue
+        if du.get_ps_plus_1(full_href) == ps1:
+            links.append(elem)
+    return links
 
 
 # ####### Search Helpers ########
@@ -157,3 +173,156 @@ def is_clickable(driver, full_xpath, xpath, timeout=1):
         return XPathUtil.is_clickable(full_xpath)
     except (TimeoutException, ElementNotVisibleException):
         return False
+
+
+def click_to_elem(element, sleep_after=0.5):
+    """Click to element and handle WebDriverException."""
+    try:
+        element.click()
+        time.sleep(sleep_after)
+    except WebDriverException:
+        pass
+
+
+def move_to_element(driver, element):
+    try:
+        ActionChains(driver).move_to_element(element).perform()
+    except WebDriverException:
+        pass
+
+
+def is_displayed(element):
+    try:
+        return element.is_displayed()
+    except (StaleElementReferenceException, WebDriverException):
+        return False
+
+
+def is_active(input_element):
+    """Check if we can interact with the given element."""
+    try:
+        return input_element.is_displayed() and input_element.is_enabled()
+    except WebDriverException:
+        return False
+
+
+def get_button_text(element):
+    """Get the text either via `value` attribute or using (inner) `text`.
+
+    `value` attribute works for <input type="button"...> or
+    <input type="submit".
+
+    `text` works for <button>elements, e.g. <button>text</button>.
+    """
+    button_text = element.get_attribute("value") or element.text
+    return button_text.lower()
+
+
+def iter_frames(driver):
+    """Return a generator for iframes."""
+    driver.switch_to_default_content()
+    iframes = driver.find_elements_by_tag_name('iframe')
+    for iframe in iframes:
+        driver.switch_to_default_content()
+        yield iframe
+    driver.switch_to_default_content()
+
+
+def switch_to_parent_frame(driver, frame_stack):
+    """Switch driver to parent frame
+
+    Selenium doesn't provide a method to switch up to a parent frame.
+    Any frame handles collected in a parent frame can't be used in the
+    child frame, so the only way to switch to a parent frame is to
+    switch back to the top-level frame and then switch back down to the
+    parent through all iframes.
+
+    Parameters
+    ----------
+    driver : selenium.webdriver
+        A Selenium webdriver instance.
+    frame_stack : list of selenium.webdriver.remote.webelement.WebElement
+        list of parent frame handles (including current frame)
+    """
+    driver.switch_to_default_content()  # start at top frame
+    # First item is 'default', last item is current frame
+    for frame in frame_stack[1:-1]:
+        driver.switch_to_frame(frame)
+
+
+def execute_in_all_frames(driver, func, kwargs={}, frame_stack=['default'],
+                          max_depth=5, logger=None, visit_id=-1):
+    """Recursively apply `func` within each iframe
+
+    When called at each level, `func` will be passed the webdriver instance
+    as an argument as well as any named arguments given in `kwargs`. If you
+    require a return value from `func` it should be stored in a mutable
+    argument. Function returns and positional arguments are not supported.
+    `func` should be defined with the following structure:
+
+    >>> def print_and_gather_links(driver, frame_stack,
+    >>>                            print_prefix='', links=[]):
+    >>>     elems = driver.find_elements_by_tag_name('a')
+    >>>     for elem in elems:
+    >>>         link = elem.get_attribute('href')
+    >>>         print print_prefix + link
+    >>>         links.append(link)
+
+    `execute_in_all_frames` should then be called as follows:
+
+    >>> all_links = list()
+    >>> execute_in_all_frames(driver, print_and_gather_links,
+    >>>                       {'prefix': 'Link ', 'links': all_links})
+    >>> print "All links on page (including all iframes):"
+    >>> print all_links
+
+    Parameters
+    ----------
+    driver : selenium.webdriver
+        A Selenium webdriver instance.
+    func : function
+        A function handle to apply to the webdriver instance within each frame
+    max_depth : int
+        Maximum depth to recurse into
+    frame_stack : list of selenium.webdriver.remote.webelement.WebElement
+        list of parent frame handles (including current frame)
+    logger : logger
+        logging module's logger
+    visit_id : int
+        ID of the visit
+
+    """
+    # Ensure we start at the top level frame
+    if len(frame_stack) == 1:
+        driver.switch_to_default_content()
+
+    # Bail if past depth cutoff
+    if len(frame_stack) - 1 > max_depth:
+        return
+
+    # Execute function in this frame
+    func(driver, frame_stack, **kwargs)
+
+    # Grab all iframes in the current frame
+    frames = driver.find_elements_by_tag_name('iframe')
+
+    # Recurse through frames
+    for frame in frames:
+        frame_stack.append(frame)
+        try:
+            driver.switch_to_frame(frame)
+        except StaleElementReferenceException:
+            if logger is not None:
+                logger.error("Error while switching to frame %s (visit: %d))" %
+                             (str(frame), visit_id))
+            continue
+        else:
+            if logger is not None:
+                doc_url = driver.execute_script("return window.document.URL;")
+                logger.info("Switched to frame: %s (visit: %d)" %
+                            (doc_url, visit_id))
+            # Search within child frame
+            execute_in_all_frames(driver, func, kwargs, frame_stack, max_depth)
+            switch_to_parent_frame(driver, frame_stack)
+        finally:
+            frame_stack.pop()
