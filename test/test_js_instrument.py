3206133cc30764d030dea3cd108ee63fd97c469b
englehardt <englehardt@gmail.com>
2018-06-11 14:18:21 -0400

    Adding frame and document urls to js instrument logs

--- test/test_js_instrument.py
+++ test/test_js_instrument.py
@@ -1,32 +1,34 @@
 from __future__ import absolute_import
 from .openwpmtest import OpenWPMTest
 from ..automation.utilities import db_utils
+from . import utilities as util
+import re
 
 GETS_AND_SETS = {
-    ("window.test.prop1", "get", "prop1"),
-    ("window.test.prop1", "set", "blah1"),
-    ("window.test.prop1", "get", "blah1"),
-    ("window.test.prop2", "get", "prop2"),
-    ("window.test.prop2", "set", "blah2"),
-    ("window.test.prop2", "get", "blah2"),
-    ("window.test.objProp", "get", "{\"hello\":\"world\"}"),
-    ("window.test.objProp", "set", "{\"key\":\"value\"}"),
-    ("window.test.objProp", "get", "{\"key\":\"value\"}"),
-    ("window.test.prop3", "get", "default-value"),
-    ("window.test.prop3", "set", "blah3"),
-    ("window.test.prop3", "get", "blah3"),
-    ('window.test.method1', 'set', 'FUNCTION'),
-    ('window.test.method1', 'set', 'now static'),
-    ('window.test.method1', 'get', 'now static'),
-    ('window.test.prop1', 'set', 'FUNCTION'),
-    ('window.test.nestedObj', 'get',
+    ("prop1", "get", "prop1"),
+    ("prop1", "set", "blah1"),
+    ("prop1", "get", "blah1"),
+    ("prop2", "get", "prop2"),
+    ("prop2", "set", "blah2"),
+    ("prop2", "get", "blah2"),
+    ("objProp", "get", "{\"hello\":\"world\"}"),
+    ("objProp", "set", "{\"key\":\"value\"}"),
+    ("objProp", "get", "{\"key\":\"value\"}"),
+    ("prop3", "get", "default-value"),
+    ("prop3", "set", "blah3"),
+    ("prop3", "get", "blah3"),
+    ('method1', 'set', 'FUNCTION'),
+    ('method1', 'set', 'now static'),
+    ('method1', 'get', 'now static'),
+    ('prop1', 'set', 'FUNCTION'),
+    ('nestedObj', 'get',
      '{"prop1":"default1","prop2":"default2","method1":"FUNCTION"}')
 }
 
 METHOD_CALLS = {
-    ('window.test.prop1', 'call', '{"0":"now accepting arugments"}'),
-    ('window.test.method1', 'call', '{"0":"hello","1":"{\\"world\\":true}"}'),
-    ('window.test.method1', 'call', '{"0":"new argument"}')
+    ('prop1', 'call', '{"0":"now accepting arugments"}'),
+    ('method1', 'call', '{"0":"hello","1":"{\\"world\\":true}"}'),
+    ('method1', 'call', '{"0":"new argument"}')
 }
 
 RECURSIVE_GETS_AND_SETS = {
@@ -69,6 +71,10 @@ SET_PREVENT_GETS_AND_SETS = {
     ('window.test3.obj1.prop2', 'get', 'newprop2'),
 }
 
+TOP_URL = u"%s/js_instrument/instrument_object.html" % util.BASE_TEST_URL
+FRAME1_URL = u"%s/js_instrument/framed1.html" % util.BASE_TEST_URL
+FRAME2_URL = u"%s/js_instrument/framed2.html" % util.BASE_TEST_URL
+
 
 class TestJSInstrument(OpenWPMTest):
 
@@ -78,34 +84,53 @@ class TestJSInstrument(OpenWPMTest):
         manager_params['testing'] = True
         return manager_params, browser_params
 
-    def test_instrument_object(self):
-        """ Ensure instrumentObject logs all property gets, sets, and calls """
-        db = self.visit('/instrument_object.html')
-        rows = db_utils.get_javascript_entries(db)
-
-        # Check calls of non-recursive instrumentation
+    def _check_calls(self, rows, symbol_prefix, doc_url, top_url):
+        """Helper to check method calls and accesses in each frame"""
         observed_gets_and_sets = set()
         observed_calls = set()
-        for script_url, symbol, operation, value, arguments in rows:
-            if not symbol.startswith('window.test.'):
+        for row in rows:
+            if not row['symbol'].startswith(symbol_prefix):
                 continue
-            if operation == 'get' or operation == 'set':
-                observed_gets_and_sets.add((symbol, operation, value))
+            symbol = re.sub(symbol_prefix, '', row['symbol'])
+            assert row['document_url'] == doc_url
+            assert row['top_level_url'] == top_url
+            if row['operation'] == 'get' or row['operation'] == 'set':
+                observed_gets_and_sets.add(
+                    (symbol, row['operation'], row['value'])
+                )
             else:
-                observed_calls.add((symbol, operation, arguments))
+                observed_calls.add(
+                    (symbol, row['operation'], row['arguments'])
+                )
         assert observed_calls == METHOD_CALLS
         assert observed_gets_and_sets == GETS_AND_SETS
+
+    def test_instrument_object(self):
+        """ Ensure instrumentObject logs all property gets, sets, and calls """
+        db = self.visit('/js_instrument/instrument_object.html')
+        rows = db_utils.get_javascript_entries(db, all_columns=True)
+
+        # Check calls of non-recursive instrumentation
+        self._check_calls(rows, 'window.test.', TOP_URL, TOP_URL)
+        self._check_calls(rows, 'window.frame1Test.', FRAME1_URL, TOP_URL)
+        self._check_calls(rows, 'window.frame2Test.', FRAME2_URL, TOP_URL)
 
         # Check calls of recursive instrumentation
         observed_gets_and_sets = set()
         observed_calls = set()
-        for script_url, symbol, operation, value, arguments in rows:
-            if not symbol.startswith('window.test2.nestedObj'):
+        for row in rows:
+            if not row['symbol'].startswith('window.test2.nestedObj'):
                 continue
-            if operation == 'get' or operation == 'set':
-                observed_gets_and_sets.add((symbol, operation, value))
+            assert row['document_url'] == TOP_URL
+            assert row['top_level_url'] == TOP_URL
+            if row['operation'] == 'get' or row['operation'] == 'set':
+                observed_gets_and_sets.add(
+                    (row['symbol'], row['operation'], row['value'])
+                )
             else:
-                observed_calls.add((symbol, operation, arguments))
+                observed_calls.add(
+                    (row['symbol'], row['operation'], row['arguments'])
+                )
         assert observed_calls == RECURSIVE_METHOD_CALLS
         assert observed_gets_and_sets == RECURSIVE_GETS_AND_SETS
 
@@ -113,21 +138,31 @@ class TestJSInstrument(OpenWPMTest):
         # We should only see the window.test2.l1.l2.l3.l4.l5.prop access
         # and not window.test2.l1.l2.l3.l4.l5.l6.prop access.
         prop_access = set()
-        for script_url, symbol, operation, value, arguments in rows:
-            if not symbol.startswith('window.test2.l1'):
+        for row in rows:
+            if not row['symbol'].startswith('window.test2.l1'):
                 continue
-            prop_access.add((symbol, operation, value))
+            assert row['document_url'] == TOP_URL
+            assert row['top_level_url'] == TOP_URL
+            prop_access.add(
+                (row['symbol'], row['operation'], row['value'])
+            )
         assert prop_access == RECURSIVE_PROP_SET
 
         # Check calls of object with sets prevented
         observed_gets_and_sets = set()
         observed_calls = set()
-        for script_url, symbol, operation, value, arguments in rows:
-            if not symbol.startswith('window.test3'):
+        for row in rows:
+            if not row['symbol'].startswith('window.test3'):
                 continue
-            if operation == 'call':
-                observed_calls.add((symbol, operation, arguments))
+            assert row['document_url'] == TOP_URL
+            assert row['top_level_url'] == TOP_URL
+            if row['operation'] == 'call':
+                observed_calls.add(
+                    (row['symbol'], row['operation'], row['arguments'])
+                )
             else:
-                observed_gets_and_sets.add((symbol, operation, value))
+                observed_gets_and_sets.add(
+                    (row['symbol'], row['operation'], row['value'])
+                )
         assert observed_calls == SET_PREVENT_CALLS
         assert observed_gets_and_sets == SET_PREVENT_GETS_AND_SETS
