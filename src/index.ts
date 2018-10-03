bdc64156de317a4f6821ca2c0b5f03e81fb402a7
Fredrik Wollsén <fredrik@neam.se>
2018-10-03 22:54:38 +0300

    refactor: Simplify module exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
-import { CookieInstrument } from "./background/cookie-instrument";
-import { HttpInstrument } from "./background/http-instrument";
-import { JavascriptInstrument } from "./background/javascript-instrument";
-
-const background = { CookieInstrument, JavascriptInstrument, HttpInstrument };
-
-import { JavascriptInstrumentContentScope } from "./content/javascript-instrument-content-scope";
-
-const content = { JavascriptInstrumentContentScope };
-
-import * as HttpPostParser from "./lib/http-post-parser";
-
-const lib = { HttpPostParser };
-
-export { background, content, lib };
+export * from "./background/cookie-instrument";
+export * from "./background/http-instrument";
+export * from "./background/javascript-instrument";
+export * from "./content/javascript-instrument-content-scope";
+export * from "./lib/http-post-parser";
