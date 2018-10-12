8f1a3248a8b0fcdab848b96b20829ff6b3aa1365
Fredrik Wollsén <fredrik@neam.se>
2018-10-12 15:43:27 +0200

    fix: Export string-utils

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./background/http-instrument";
 export * from "./background/javascript-instrument";
 export * from "./content/javascript-instrument-content-scope";
 export * from "./lib/http-post-parser";
+export * from "./lib/string-utils";
