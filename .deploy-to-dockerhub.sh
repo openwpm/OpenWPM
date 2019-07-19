c6fa6d3c78c607d1eb6358bf74ea5f3841d9abd4
Fredrik Wollsén <fredrik@neam.se>
2019-07-19 14:55:43 +0300

    Set deploy script executable and only attempt dockerhub deploy if docker user env var is set

--- .deploy-to-dockerhub.sh
+++ .deploy-to-dockerhub.sh
@@ -1,18 +1,20 @@
-echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-if [ "${TRAVIS_BRANCH}" == "master" ]; then
-  # deploy master
-  docker tag app:build ${DOCKERHUB_REPO}:latest
-  docker push ${DOCKERHUB_REPO}:latest
-elif  [ ! -z "${TRAVIS_TAG}" ]; then
-  # deploy a release tag...
-  echo "${DOCKERHUB_REPO}:${TRAVIS_TAG}"
-  docker tag app:build "${DOCKERHUB_REPO}:${TRAVIS_TAG}"
-  docker images
-  docker push "${DOCKERHUB_REPO}:${TRAVIS_TAG}"
-elif  [ ! -z "${TRAVIS_COMMIT}" ]; then
-  # deploy a commit tag...
-  echo "${DOCKERHUB_REPO}:commit-${TRAVIS_COMMIT}"
-  docker tag app:build "${DOCKERHUB_REPO}:commit-${TRAVIS_COMMIT}"
-  docker images
-  docker push "${DOCKERHUB_REPO}:commit-${TRAVIS_COMMIT}"
+if [ "${DOCKER_USER}" != "" ]; then
+    echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+    if [ "${TRAVIS_BRANCH}" == "master" ]; then
+      # deploy master
+      docker tag app:build ${DOCKERHUB_REPO}:latest
+      docker push ${DOCKERHUB_REPO}:latest
+    elif  [ ! -z "${TRAVIS_TAG}" ]; then
+      # deploy a release tag...
+      echo "${DOCKERHUB_REPO}:${TRAVIS_TAG}"
+      docker tag app:build "${DOCKERHUB_REPO}:${TRAVIS_TAG}"
+      docker images
+      docker push "${DOCKERHUB_REPO}:${TRAVIS_TAG}"
+    elif  [ ! -z "${TRAVIS_COMMIT}" ]; then
+      # deploy a commit tag...
+      echo "${DOCKERHUB_REPO}:commit-${TRAVIS_COMMIT}"
+      docker tag app:build "${DOCKERHUB_REPO}:commit-${TRAVIS_COMMIT}"
+      docker images
+      docker push "${DOCKERHUB_REPO}:commit-${TRAVIS_COMMIT}"
+    fi
 fi
