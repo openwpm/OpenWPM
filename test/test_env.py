0e76f6324419a1cf219a798220b3da33f887fa12
Sarah Bird <birdsarah@mozilla.com>
2019-07-30 01:05:48 -0500

    Update test_env

--- test/test_env.py
+++ test/test_env.py
@@ -15,20 +15,4 @@ class TestDependencies(OpenWPMTest):
     def test_dependencies(self):
         self.assert_is_installed("npm")
         firefox_binary_path = get_firefox_binary_path()
-        geckodriver_executable_path = get_geckodriver_exec_path()
         assert isfile(firefox_binary_path)
-        assert isfile(geckodriver_executable_path)
-
-    def check_py_pkgs(self, requirements_file):
-        assert isfile(requirements_file)
-        for line in open(requirements_file):
-            line = line.strip()
-            if line == "" or line[0] == "#":
-                continue
-            pkg = re.split(r'[>=<]', line)[0]
-            print("Checking Python package", pkg)
-            self.assert_py_pkg_installed(pkg)
-
-    def test_py_pkgs(self):
-        self.check_py_pkgs(join(self.BASE_DIR, "requirements.txt"))
-        self.check_py_pkgs(join(self.BASE_DIR, "requirements-dev.txt"))
