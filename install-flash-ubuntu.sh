443fcc1ce450d2fe713b81516fb501102fb54b04
Sarah Bird <birdsarah@mozilla.com>
2019-07-30 00:46:14 -0500

    Scripts should be executable, don't need to check flash install

--- install-flash-ubuntu.sh
+++ install-flash-ubuntu.sh
@@ -1,43 +1,5 @@
 #!/bin/bash
 set -e
-
-if [[ $# -gt 1 ]]; then
-    echo "Usage: install-flash-ubuntu.sh [--flash | --no-flash]" >&2
-    exit 1
-fi
-
-if [[ $# -gt 0 ]]; then
-    case "$1" in
-        "--flash")
-            flash=true
-            ;;
-        "--no-flash")
-            flash=false
-            ;;
-        *)
-            echo "Usage: install.sh [--flash | --no-flash]" >&2
-            exit 1
-            ;;
-    esac
-else
-    echo "Would you like to install Adobe Flash Player? (Only required for crawls with Flash) [y,N]"
-    read -s -n 1 response
-    if [[ $response = "" ]] || [ $response == 'n' ] || [ $response == 'N' ]; then
-        flash=false
-        echo Not installing Adobe Flash Plugin
-    elif [ $response == 'y' ] || [ $response == 'Y' ]; then
-        flash=true
-        echo Installing Adobe Flash Plugin
-    else
-        echo Unrecognized response, exiting
-        exit 1
-    fi
-fi
-if [ "$flash" = true ]; then
-    sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
-fi
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
 sudo apt-get update
-if [ "$flash" = true ]; then
-    sudo apt-get install -y adobe-flashplugin
-fi
-dd
+sudo apt-get install -y adobe-flashplugin
