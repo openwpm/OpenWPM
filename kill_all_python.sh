cf011814803af09edf8b9644d0e1a19e7b5badf2
vringar <szabka@mozilla.com>
2019-11-26 15:06:26 +0100

    trying different script

--- kill_all_python.sh
+++ kill_all_python.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+set -o errexit
 pgrep python | xargs -n 1 kill
-while [[pgrep python ]]
+echo "Test"
+while true
 do
-    sleep(100)
+  pgrep python > /dev/null;
+  x=$?
+  echo "$x was the last status"
+  if [ $x -eq 0 ]; then
+    wait 100
+  else
+    break
+  fi
 done
 echo "Successfully shut down"
