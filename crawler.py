d41ff52bdeaaa6c9bb8a35d6060261c791b2ba9f
Kainaat Singh <kainaat.singh@gmail.com>
2020-03-10 13:02:23 +0530

    setting site_rank

--- crawler.py
+++ crawler.py
@@ -126,7 +126,8 @@ while not job_queue.empty():
         site = "http://" + site
     manager.logger.info("Visiting %s..." % site)
     command_sequence = CommandSequence.CommandSequence(
-        site, blocking=True, reset=True, retry_number=retry_number
+        site, blocking=True, reset=True, retry_number=retry_number,
+        site_rank=site_rank
     )
     command_sequence.get(sleep=DWELL_TIME, timeout=TIMEOUT)
     manager.execute_command_sequence(command_sequence)
