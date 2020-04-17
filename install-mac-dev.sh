9b4dedbcfaad59354063101c10225181e127460a
Sarah Bird <birdsarah@mozilla.com>
2020-04-17 13:07:33 -0500

    wip updating mac-dev dependencies

--- install-mac-dev.sh
+++ install-mac-dev.sh
@@ -1,26 +1,7 @@
-#!/bin/bash
-set -e
-
-# Note: This install script assumes that node and python is already setup
-# and is meant for setting up local development rather than CI environments
-
-if [[ $# -gt 0 ]]; then
-    echo "Usage: install-mac.sh" >&2
-    exit 1
-fi
-
-# Create and activate a local Python 3 venv
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-
 # Install all requirements except plyvel
 cat requirements.txt | grep -v plyvel > requirements.mac.txt
 CFLAGS='-mmacosx-version-min=10.7 -stdlib=libc++' pip install -r requirements.mac.txt
 rm requirements.mac.txt
-
-# A recent version of leveldb is required
-brew install leveldb || brew upgrade leveldb
 
 # Make sure we build plyvel properly to work with the installed leveldb on recent OSX versions
 CFLAGS='-mmacosx-version-min=10.7 -stdlib=libc++ -std=c++11' pip install --force-reinstall --ignore-installed --no-binary :all: plyvel
