b4d689905a20f22597d1ea5b9f9d001fb7b35da1
Sarah Bird <birdsarah@mozilla.com>
2020-05-13 15:41:52 -0500

    Make one install-firefox script

--- install-firefox-mac.sh
+++
@@ -1,9 +0,0 @@
-TAG=25e0edbb0a613c3bf794c93ba3aa0985d29d5ef4
-UNBRANDED_RELEASE_MAC_BUILD="https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.mozilla-release.revision.$TAG.firefox.macosx64-add-on-devel/artifacts/public/build/target.dmg"
-wget "$UNBRANDED_RELEASE_MAC_BUILD"
-# Install unbranded release ff
-rm -rf Nightly.app || true
-hdiutil attach -nobrowse -mountpoint /Volumes/firefox-tmp target.dmg
-cp -r /Volumes/firefox-tmp/Nightly.app .
-hdiutil detach /Volumes/firefox-tmp
-rm target.dmg
