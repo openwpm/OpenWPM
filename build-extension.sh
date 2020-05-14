3253fe7a890ba8d524372cf885f8f33d5284508c
Sarah Bird <birdsarah@mozilla.com>
2020-05-13 22:23:36 -0500

    Make sure build extension uses conda node

--- build-extension.sh
+++ build-extension.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+
+# This script builds and installs the
+# webextension instrumentation.
+
 set -e
 
-cd automation/Extension/firefox
+# Make conda available to shell script
+eval "$(conda shell.bash hook)"
+conda activate openwpm
+
+echo '**********************xxxxxxxxxxxxxxx'
+node --version
+
+pushd automation/Extension/firefox
 npm install
 npm run build
 cp dist/*.zip ./openwpm.xpi
+popd
 
 echo "Success: automation/Extension/firefox/openwpm.xpi has been built"
