e77edfacfc0a9ab13ec50a67b044a2d38c39189b
Sarah Bird <birdsarah@mozilla.com>
2020-05-13 19:10:15 -0500

    Add dev dependencies to environment

--- repin.sh
+++ repin.sh
@@ -12,8 +12,11 @@ eval "$(conda shell.bash hook)"
 # Create openwpm env with unpinned yaml file
 conda env create --force -q -f environment-unpinned.yaml
 
+# Adding dev dependencies to environment
+conda env update -n openwpm -f environment-unpinned-dev.yaml
+
 # Export the environment including manually specify channels
-conda env export --override-channels -n openwpm -c conda-forge -c main -f environment.yaml
+conda env export -n openwpm --override-channels -c conda-forge -c main -f environment.yaml
 
 # Remove prefix line from end of export (it doesn't actually
 # have an impact, but it's confusing so this gives us a cleaner
