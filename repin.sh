705de01e0330ca9e4a2e8976474b04e932463adc
Sarah Bird <birdsarah@mozilla.com>
2020-05-14 00:14:30 -0500

    Try a pruning strategy
    Alternative cross-platform strategy

--- repin.sh
+++ repin.sh
@@ -4,19 +4,6 @@
 # This script will remove an existing openwpm
 # conda environment if it exists.
 
-case "$(uname -s)" in
-  Darwin)
-    ENV_SUFFIX='-macosx'
-    ;;
-  Linux)
-    ENV_SUFFIX=''
-    ;;
-  *)
-    echo 'Your OS is not supported. Aborting'
-    exit 1
-    ;;
-esac
-
 set -e
 
 # Make conda available to shell script
@@ -25,15 +12,16 @@ eval "$(conda shell.bash hook)"
 # Create openwpm env with unpinned yaml file
 conda env create --force -q -f environment-unpinned.yaml
 
+# Activate
+conda activate openwpm
+
 # Adding dev dependencies to environment
-conda env update -n openwpm -f environment-unpinned-dev.yaml
+conda env update -f environment-unpinned-dev.yaml
 
 # Export the environment including manually specify channels
-conda env export -n openwpm --no-builds --override-channels -c conda-forge -c main -f environment$ENV_SUFFIX.yaml
+conda env export --no-builds --override-channels -c conda-forge -c main -f environment.yaml
 
-# Remove prefix line from end of export (it doesn't actually
-# have an impact, but it's confusing so this gives us a cleaner
-# environment.yaml)
-sed -i '$d' environment.yaml
+# Prune environment file to only things we want to pin
+python prune-environment.py
 
 echo 'New environment.yaml successfully created.'
