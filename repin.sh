c497f89277e6b64e9447e07fc225b53eb8af7840
Sarah Bird <birdsarah@mozilla.com>
2020-05-13 23:06:27 -0500

    Repin per OS

--- repin.sh
+++ repin.sh
@@ -4,6 +4,19 @@
 # This script will remove an existing openwpm
 # conda environment if it exists.
 
+case "$(uname -s)" in
+  Darwin)
+    ENV_SUFFIX='-macosx'
+    ;;
+  Linux)
+    ENV_SUFFIX=''
+    ;;
+  *)
+    echo 'Your OS is not supported. Aborting'
+    exit 1
+    ;;
+esac
+
 set -e
 
 # Make conda available to shell script
@@ -16,7 +29,7 @@ conda env create --force -q -f environment-unpinned.yaml
 conda env update -n openwpm -f environment-unpinned-dev.yaml
 
 # Export the environment including manually specify channels
-conda env export -n openwpm --no-builds --override-channels -c conda-forge -c main -f environment.yaml
+conda env export -n openwpm --no-builds --override-channels -c conda-forge -c main -f environment$ENV_SUFFIX.yaml
 
 # Remove prefix line from end of export (it doesn't actually
 # have an impact, but it's confusing so this gives us a cleaner
