cd4b0f713de1d498586af8bdb37fb6aed5f2847b
Sarah Bird <birdsarah@mozilla.com>
2020-05-21 02:38:03 -0500

    Latest py tests

--- test/conftest.py
+++ test/conftest.py
@@ -15,20 +15,18 @@ EXTENSION_DIR = os.path.join(
     'firefox')
 
 
-def create_xpi():
-    """Creates a new xpi using npm run build."""
-    if utilities.which("npm"):
-        subprocess.check_call(["npm", "run", "build"],
-                              cwd=EXTENSION_DIR)
-    else:
-        assert os.path.exists(os.path.join(EXTENSION_DIR, 'openwpm.xpi'))
-
-
 @pytest.fixture(scope="session", autouse=True)
 def prepare_test_setup(request):
     """Run an HTTP server during the tests."""
-    create_xpi()
-    print("\nStarting local_http_server")
+
+    if 'pyonly' in request.config.invocation_params.args:
+        return
+
+    # Creates a new xpi using npm run build.
+    print("Building new xpi")
+    subprocess.check_call(["npm", "run", "build"], cwd=EXTENSION_DIR)
+
+    print("Starting local_http_server")
     server, server_thread = utilities.start_server()
 
     def local_http_server_stop():
--- test/test_js_instrument_py.py
+++ test/test_js_instrument_py.py
@@ -1,47 +1,139 @@
+import pytest
+from jsonschema.exceptions import ValidationError
 from ..automation import JSInstrumentation as jsi
 
-# Unit tests to test JSInstrumentation.py
-# which exists to process short-cut APIs
-# assert Falseed to browser_params['js_instrumentation_modules']
-# and to validate the string to be assert Falseed to the instrumentation
+pytestmark = pytest.mark.pyonly
 
-def test_validate_good_string():
-    good_string = """
-        [
+
+def _no_whitespace(x):
+    return "".join(x.split())
+
+# Test function that converts our python
+# objects to our JS string
+
+def test_python_to_js_lower_true_false():
+    inpy = [{
+        'object': 'window',
+        'logSettings': {
+            'logCallStack': False,
+            'preventSets': True,
+        }
+    }]
+    expected_out = _no_whitespace("""
+    [{
+        "object": window,
+        "logSettings": {
+            "logCallStack": false,
+            "preventSets": true
+        }
+    }]
+    """)
+    actual_out = _no_whitespace(jsi.python_to_js_string(inpy))
+    assert actual_out == expected_out
+
+
+def test_python_to_js_no_quote_object():
+    inpy = [{'object': 'window', 'logSettings': {}}]
+    expected_out = _no_whitespace('[{"object": window, "logSettings": {}}]')
+    actual_out = _no_whitespace(jsi.python_to_js_string(inpy))
+    assert actual_out == expected_out
+
+
+def test_python_to_js_no_quote_object_two_matching_objects():
+    inpy = [
+        {'object': 'window', 'logSettings': {}},
+        {'object': 'window', 'logSettings': {}},
+        {'object': 'window2', 'logSettings': {}}
+    ]
+    expected_out = _no_whitespace("""[
+        {"object": window, "logSettings": {}},
+        {"object": window, "logSettings": {}},
+        {"object": window2, "logSettings": {}}
+    ]""")
+    actual_out = _no_whitespace(jsi.python_to_js_string(inpy))
+    assert actual_out == expected_out
+
+
+# Test our validation
+@pytest.fixture
+def default_log_settings():
+    return {
+        'propertiesToInstrument': [],
+        'nonExistingPropertiesToInstrument': [],
+        'excludedProperties': [],
+        'logCallStack': False,
+        'logFunctionsAsStrings': False,
+        'logFunctionGets': False,
+        'preventSets': False,
+        'recursive': False,
+        'depth': 5,
+    }
+
+
+def test_validate_good(default_log_settings):
+    good_input = [
+        {
+            'object': 'window',
+            'instrumentedName': "window",
+            'logSettings': default_log_settings
+        }
+    ]
+    assert jsi.validate(good_input)
+
+
+def test_validate_bad__log_settings_missing(default_log_settings):
+    log_keys = default_log_settings.keys()
+    for log_key in log_keys:
+        print('Testing for missing', log_key)
+        log_settings = default_log_settings.copy()
+        log_settings.pop(log_key)
+        bad_input = [
             {
-                object: window,
-                instrumentedName: "window",
-                logSettings: {
-                    propertiesToInstrument: ["partiallyExisting",]
-                    nonExistingPropertiesToInstrument: [],
-                    excludedProperties: [],
-                    logCallStack: false,
-                    logFunctionsAsStrings: false,
-                    logFunctionGets: false,
-                    preventSets: false,
-                    recursive: false,
-                    depth: 5,
-                },
+                'object': 'window',
+                'instrumentedName': "window",
+                'logSettings': log_settings
             }
         ]
-    """
-    assert jsi.validate(good_string)
+        with pytest.raises(ValidationError):
+            jsi.validate(bad_input),
 
+def test_validate_bad__log_settings_invalid(default_log_settings):
+    log_settings = default_log_settings.copy()
+    log_settings['recursive'] = 'yes, please'
+    bad_input = [
+        {
+            'object': 'window',
+            'instrumentedName': "window",
+            'logSettings': log_settings
+        }
+    ]
+    with pytest.raises(ValidationError):
+        assert jsi.validate(bad_input)
 
-def test_validate_bad_string_log_settings_missing():
-    assert False
+def test_validate_bad__not_a_list(default_log_settings):
+    bad_input = {
+        'object': 'window',
+        'instrumentedName': "window",
+        'logSettings': default_log_settings
+    }
+    with pytest.raises(ValidationError):
+        assert jsi.validate(bad_input)
 
-def test_validate_bad_string_log_settings_invalid():
-    assert False
+def test_validate_bad__missing_object(default_log_settings):
+    bad_input = [{
+        'instrumentedName': "window",
+        'logSettings': default_log_settings
+    }]
+    with pytest.raises(ValidationError):
+        assert jsi.validate(bad_input)
 
-def test_validate_bad_string_not_a_list():
-    assert False
-
-def test_validate_bad_string_missing_object():
-    assert False
-
-def test_validated_bad_string_missing_instrumentedName():
-    assert False
+def test_validated_bad__missing_instrumentedName():
+    bad_input = [{
+        'object': 'window',
+        'logSettings': default_log_settings
+    }]
+    with pytest.raises(ValidationError):
+        assert jsi.validate(bad_input)
 
 def test_deduplication_multiple_duped_properties():
     assert False
