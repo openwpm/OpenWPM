5b80acd75f287353b7ed37bd08158825d031ce04
vringar <szabka@mozilla.com>
2020-11-18 19:47:12 +0100

    Fixed test

--- test/test_timer.py
+++ test/test_timer.py
@@ -20,7 +20,7 @@ class TestCommandDuration(OpenWPMTest):
 
         get_command = db_utils.query_db(
             manager_params["db"],
-            "SELECT duration FROM crawl_history WHERE command = \"<class 'OpenWPM.openwpm.Commands.Types.GetCommand'>\"",
+            "SELECT duration FROM crawl_history WHERE command = \"<class 'openwpm.Commands.Types.GetCommand'>\"",
             as_tuple=True,
         )[0]
 
