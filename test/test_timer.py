0d3bd4bf7aacfdc0c4cabb7df877416ee50f2b32
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-11-18 15:27:22 +0530

    making the test run locally without internet

--- test/test_timer.py
+++ test/test_timer.py
@@ -1,6 +1,10 @@
 from ..openwpm import TaskManager
 from ..openwpm.utilities import db_utils
 from .openwpmtest import OpenWPMTest
+from .utilities import BASE_TEST_URL
+
+TEST_FILE = "canvas_fingerprinting.html"
+TEST_URL = BASE_TEST_URL + "/" + TEST_FILE
 
 
 class TestCommandDuration(OpenWPMTest):
@@ -10,16 +14,16 @@ class TestCommandDuration(OpenWPMTest):
     def test_command_duration(self):
         manager_params, browser_params = self.get_config()
         manager = TaskManager.TaskManager(manager_params, browser_params)
-        manager.get(url="http://www.example.com", sleep=5)
+        manager.get(url=TEST_URL, sleep=5)
         manager.close()
 
         get_command = db_utils.query_db(
             manager_params["db"],
-            "SELECT duration " "FROM crawl_history ",
+            "SELECT duration FROM crawl_history WHERE command = \"<class 'OpenWPM.openwpm.Commands.Types.GetCommand'>\"",
             as_tuple=True,
-        )
+        )[0]
 
-        assert get_command[1][0] > (5 * 1000)  # milliseconds conversion for sleep time
-        assert get_command[1][0] <= (
-            (5 * 1000) + 3 * 1000
+        assert get_command[0] > (5 * 1000)  # milliseconds conversion for sleep time
+        assert get_command[0] <= (
+            (5 * 1000) + 2 * 1000
         )  # milliseconds conversion for sleep time + time duration a command took (milliseconds)
