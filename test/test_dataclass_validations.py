0cc043128f856f0d7fc9c5a998159253332e08aa
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-12-01 14:46:36 +0530

    fixed some tests

--- test/test_dataclass_validations.py
+++ test/test_dataclass_validations.py
@@ -1,112 +1,115 @@
 import pytest
 
-from openwpm.config import validate_browser_params, validate_manager_params
+from openwpm.config import (
+    BrowserParams,
+    ManagerParams,
+    validate_browser_params,
+    validate_manager_params,
+)
 from openwpm.errors import ConfigError
 
 from .openwpmtest import OpenWPMTest
 
 
-class TestDataclassValidations(OpenWPMTest):
-    def get_config(self, data_dir=""):
-        return self.get_test_config(data_dir)
+def get_config(data_dir=""):
+    return ManagerParams(), [BrowserParams()]
 
 
-class TestBrowserParams(TestDataclassValidations):
-    def test_display_mode(self):
-        _, browser_params = self.get_config()
-        browser_params[0].display_mode = "something unsupported"
-        with pytest.raises(ConfigError):
-            validate_browser_params(browser_params[0])
-        browser_params[0].display_mode = []
-        with pytest.raises(ConfigError):
-            validate_browser_params(browser_params[0])
-        browser_params[0].display_mode = "native"
-        try:
-            validate_browser_params(browser_params[0])
-        except:
-            pytest.fail("Unexpected error raised")
+def test_display_mode():
+    _, browser_params = get_config()
 
-    def test_browser_type(self):
-        _, browser_params = self.get_config()
-        browser_params[0].browser = "something unsupported"
-        with pytest.raises(ConfigError):
-            validate_browser_params(browser_params[0])
-        browser_params[0].browser = "firefox"
-        try:
-            validate_browser_params(browser_params[0])
-        except:
-            pytest.fail("Unexpected error raised")
+    browser_params[0].display_mode = "something unsupported"
+    with pytest.raises(ConfigError):
+        validate_browser_params(browser_params[0])
 
-    def test_tp_cookies_opt(self):
-        _, browser_params = self.get_config()
-        browser_params[0].tp_cookies = "something unsupported"
-        with pytest.raises(ConfigError):
-            validate_browser_params(browser_params[0])
-        browser_params[0].tp_cookies = "never"
-        try:
-            validate_browser_params(browser_params[0])
-        except:
-            pytest.fail("Unexpected error raised")
+    browser_params[0].display_mode = []
+    with pytest.raises(ConfigError):
+        validate_browser_params(browser_params[0])
 
-    def test_save_content_type(self):
-        _, browser_params = self.get_config()
-        browser_params[0].save_content = []
-        with pytest.raises(ConfigError):
-            validate_browser_params(browser_params[0])
-        browser_params[0].save_content = "something unsupported"
-        with pytest.raises(ConfigError):
-            validate_browser_params(browser_params[0])
-        browser_params[0].save_content = False
-        try:
-            validate_browser_params(browser_params[0])
-        except:
-            pytest.fail("Unexpected error raised")
-        browser_params[0].save_content = "script"
-        try:
-            validate_browser_params(browser_params[0])
-        except:
-            pytest.fail("Unexpected error raised")
+    browser_params[0].display_mode = "native"
+    validate_browser_params(browser_params[0])
 
 
-class TestManagerParams(TestDataclassValidations):
-    def test_log_file_extension(self):
-        manager_params, _ = self.get_config()
-        manager_params.log_file = "something.unsupported"
-        with pytest.raises(ConfigError):
-            validate_manager_params(manager_params)
-        manager_params.log_file = []
-        with pytest.raises(ConfigError):
-            validate_manager_params(manager_params)
+def test_browser_type():
+    _, browser_params = get_config()
 
-    def test_database_file_extension(self):
-        manager_params, _ = self.get_config()
-        manager_params.database_name = "something.unsupported"
-        with pytest.raises(ConfigError):
-            validate_manager_params(manager_params)
+    browser_params[0].browser = "something unsupported"
+    with pytest.raises(ConfigError):
+        validate_browser_params(browser_params[0])
 
-    def test_failure_limit(self):
-        manager_params, _ = self.get_config()
-        manager_params.failure_limit = "not None and not int"
-        with pytest.raises(ConfigError):
-            validate_manager_params(manager_params)
-        manager_params.failure_limit = None  # when failure_limit is set to None
-        try:
-            validate_manager_params(manager_params)
-        except:
-            pytest.fail("Unexpected error raised")
-        manager_params.failure_limit = 2  # when failure_limit is set to int
-        try:
-            validate_manager_params(manager_params)
-        except:
-            pytest.fail("Unexpected error raised")
+    browser_params[0].browser = "firefox"
+    validate_browser_params(browser_params[0])
 
-    def test_output_format(self):
-        manager_params, _ = self.get_config()
-        manager_params.output_format = "not None and not int"
-        with pytest.raises(ConfigError):
-            validate_manager_params(manager_params)
-        manager_params.output_format = "s3"
-        try:
-            validate_manager_params(manager_params)
-        except:
-            pytest.fail("Unexpected error raised")
+
+def test_tp_cookies_opt():
+    _, browser_params = get_config()
+
+    browser_params[0].tp_cookies = "something unsupported"
+    with pytest.raises(ConfigError):
+        validate_browser_params(browser_params[0])
+
+    browser_params[0].tp_cookies = "never"
+    validate_browser_params(browser_params[0])
+
+
+def test_save_content_type():
+    _, browser_params = get_config()
+
+    browser_params[0].save_content = []
+    with pytest.raises(ConfigError):
+        validate_browser_params(browser_params[0])
+
+    browser_params[0].save_content = "something unsupported"
+    with pytest.raises(ConfigError):
+        validate_browser_params(browser_params[0])
+
+    browser_params[0].save_content = False
+    validate_browser_params(browser_params[0])
+
+    browser_params[0].save_content = "script"
+    validate_browser_params(browser_params[0])
+
+
+def test_log_file_extension():
+    manager_params, _ = get_config()
+
+    manager_params.log_file = "something.unsupported"
+    with pytest.raises(ConfigError):
+        validate_manager_params(manager_params)
+
+    manager_params.log_file = []
+    with pytest.raises(ConfigError):
+        validate_manager_params(manager_params)
+
+
+def test_database_file_extension():
+    manager_params, _ = get_config()
+
+    manager_params.database_name = "something.unsupported"
+    with pytest.raises(ConfigError):
+        validate_manager_params(manager_params)
+
+
+def test_failure_limit():
+    manager_params, _ = get_config()
+
+    manager_params.failure_limit = "not None and not int"
+    with pytest.raises(ConfigError):
+        validate_manager_params(manager_params)
+
+    manager_params.failure_limit = None  # when failure_limit is set to None
+    validate_manager_params(manager_params)
+
+    manager_params.failure_limit = 2  # when failure_limit is set to int
+    validate_manager_params(manager_params)
+
+
+def test_output_format():
+    manager_params, _ = get_config()
+
+    manager_params.output_format = "not None and not int"
+    with pytest.raises(ConfigError):
+        validate_manager_params(manager_params)
+
+    manager_params.output_format = "s3"
+    validate_manager_params(manager_params)
