07eb19d47ae4742786658c443edd4d98e29c1c4f
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-12-01 15:41:51 +0530

    fixed dataclases validation tests

--- test/test_dataclass_validations.py
+++ test/test_dataclass_validations.py
@@ -11,67 +11,63 @@ from openwpm.errors import ConfigError
 from .openwpmtest import OpenWPMTest
 
 
-def get_config(data_dir=""):
-    return ManagerParams(), [BrowserParams()]
-
-
 def test_display_mode():
-    _, browser_params = get_config()
+    browser_params = BrowserParams()
 
-    browser_params[0].display_mode = "something unsupported"
+    browser_params.display_mode = "something unsupported"
     with pytest.raises(ConfigError):
-        validate_browser_params(browser_params[0])
+        validate_browser_params(browser_params)
 
-    browser_params[0].display_mode = []
+    browser_params.display_mode = []
     with pytest.raises(ConfigError):
-        validate_browser_params(browser_params[0])
+        validate_browser_params(browser_params)
 
-    browser_params[0].display_mode = "native"
-    validate_browser_params(browser_params[0])
+    browser_params.display_mode = "native"
+    validate_browser_params(browser_params)
 
 
 def test_browser_type():
-    _, browser_params = get_config()
+    browser_params = BrowserParams()
 
-    browser_params[0].browser = "something unsupported"
+    browser_params.browser = "something unsupported"
     with pytest.raises(ConfigError):
-        validate_browser_params(browser_params[0])
+        validate_browser_params(browser_params)
 
-    browser_params[0].browser = "firefox"
-    validate_browser_params(browser_params[0])
+    browser_params.browser = "firefox"
+    validate_browser_params(browser_params)
 
 
 def test_tp_cookies_opt():
-    _, browser_params = get_config()
+    browser_params = BrowserParams()
 
-    browser_params[0].tp_cookies = "something unsupported"
+    browser_params.tp_cookies = "something unsupported"
     with pytest.raises(ConfigError):
-        validate_browser_params(browser_params[0])
+        validate_browser_params(browser_params)
 
-    browser_params[0].tp_cookies = "never"
-    validate_browser_params(browser_params[0])
+    browser_params.tp_cookies = "never"
+    validate_browser_params(browser_params)
 
 
 def test_save_content_type():
-    _, browser_params = get_config()
+    browser_params = BrowserParams()
 
-    browser_params[0].save_content = []
+    browser_params.save_content = []
     with pytest.raises(ConfigError):
-        validate_browser_params(browser_params[0])
+        validate_browser_params(browser_params)
 
-    browser_params[0].save_content = "something unsupported"
+    browser_params.save_content = "something unsupported"
     with pytest.raises(ConfigError):
-        validate_browser_params(browser_params[0])
+        validate_browser_params(browser_params)
 
-    browser_params[0].save_content = False
-    validate_browser_params(browser_params[0])
+    browser_params.save_content = False
+    validate_browser_params(browser_params)
 
-    browser_params[0].save_content = "script"
-    validate_browser_params(browser_params[0])
+    browser_params.save_content = "script"
+    validate_browser_params(browser_params)
 
 
 def test_log_file_extension():
-    manager_params, _ = get_config()
+    manager_params = ManagerParams()
 
     manager_params.log_file = "something.unsupported"
     with pytest.raises(ConfigError):
@@ -83,7 +79,7 @@ def test_log_file_extension():
 
 
 def test_database_file_extension():
-    manager_params, _ = get_config()
+    manager_params = ManagerParams()
 
     manager_params.database_name = "something.unsupported"
     with pytest.raises(ConfigError):
@@ -91,7 +87,7 @@ def test_database_file_extension():
 
 
 def test_failure_limit():
-    manager_params, _ = get_config()
+    manager_params = ManagerParams()
 
     manager_params.failure_limit = "not None and not int"
     with pytest.raises(ConfigError):
@@ -105,7 +101,7 @@ def test_failure_limit():
 
 
 def test_output_format():
-    manager_params, _ = get_config()
+    manager_params = ManagerParams()
 
     manager_params.output_format = "not None and not int"
     with pytest.raises(ConfigError):
