774e4d6feec470a8b4af4f071c002eb1b8765eff
Mollie Bakal <bakalm@umich.edu>
2020-12-13 17:33:16 -0500

    added a test case to ensure xvfb runs

---
+++ test/test_xvfb_browser.py
@@ -0,0 +1,33 @@
+from functools import partial
+from typing import List
+
+from openwpm.command_sequence import CommandSequence
+from openwpm.task_manager import TaskManager
+
+from .openwpmtest import OpenWPMTest
+from .utilities import BASE_TEST_URL
+
+
+class TestXVFBDisplay(OpenWPMTest):
+    """Test the XVFB display option to see if it runs and shuts down properly after a command is given"""
+
+    def get_config(self, data_dir=""):
+        return self.get_test_config(data_dir, display_mode="xvfb")
+
+    def test_display_shutdown(self):
+        manager_params, browser_params = self.get_config()
+        TEST_SITE = BASE_TEST_URL + "/test_pages/simple_a.html"
+        manager = TaskManager(manager_params, browser_params)
+
+        def callback(argument: List[int], success: bool):
+            argument.extend([1, 2, 3])
+
+        my_list = []
+        sequence = CommandSequence(
+            TEST_SITE, reset=True, blocking=True, callback=partial(callback, my_list)
+        )
+        sequence.get()
+
+        manager.execute_command_sequence(sequence)
+        manager.close()
+        assert my_list == [1, 2, 3]
