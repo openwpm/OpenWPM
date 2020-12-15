e15acf8aeba357b0f4af1dbb20d1b42929b8a20d
Mollie Bakal <bakalm@umich.edu>
2020-12-15 17:35:41 -0500

    updated test to check for lockfile

--- test/test_xvfb_browser.py
+++ test/test_xvfb_browser.py
@@ -1,3 +1,4 @@
+import os
 from functools import partial
 from typing import List
 
@@ -10,12 +11,15 @@ from .utilities import BASE_TEST_URL
 
 
 class ExceptionCommand(BaseCommand):
+    def __init__():
+        None
+
     def execute(self):
         raise Exception
 
 
 class TestXVFBDisplay(OpenWPMTest):
-    """Test the XVFB display option to see if it runs and shuts down properly after a command is given"""
+    """Test the XVFB display option to see if it runs and deletes the lockfile upon shutdown"""
 
     def get_config(self, data_dir=""):
         return self.get_test_config(data_dir, display_mode="xvfb")
@@ -24,9 +28,11 @@ class TestXVFBDisplay(OpenWPMTest):
         manager_params, browser_params = self.get_config()
         TEST_SITE = BASE_TEST_URL + "/test_pages/simple_a.html"
         manager = TaskManager(manager_params, browser_params)
+        port = manager.browsers[0].display_port
 
         sequence = CommandSequence(TEST_SITE)
         sequence.get()
         sequence.append_command(ExceptionCommand)
         manager.execute_command_sequence(sequence)
         manager.close()
+        assert not os.path.exists("/tmp/.X%s-lock" % port)
