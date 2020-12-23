ef0ba1e50a0c49e96d3f66f47168237e9f4ea405
vringar <szabka@mozilla.com>
2020-12-23 15:22:45 +0100

    New test

--- test/storage/test_arrow_cache.py
+++ test/storage/test_arrow_cache.py
@@ -21,21 +21,22 @@ async def test_arrow_cache(mp_logger: MPLogger) -> None:
     site_visit = TEST_VALUES["site_visits"]
     d: Dict[VisitId, Awaitable[None]] = {}
     for i in range(CACHE_SIZE + 1):
-        visit_id = VisitId(random.randint(0, 2 ** 63 - 1))
+        visit_id = VisitId(i)
         site_visit["visit_id"] = visit_id
         await prov.store_record(TableName("site_visits"), visit_id, site_visit)
         d[visit_id] = await prov.finalize_visit_id(visit_id)
 
-    handle = prov.handle
-
-    # The queue should not be empty at this point
-    handle.poll_queue(block=False)
-
     for visit_id in d:
         task = d[visit_id]
         await asyncio.wait_for(task, 1)
+    await asyncio.sleep(1)
+    handle = prov.handle
+    # The queue should not be empty at this point
+    handle.poll_queue(block=False)
 
     assert len(handle.storage["site_visits"]) == 1
     table = handle.storage["site_visits"][0]
 
     df: DataFrame = table.to_pandas()
+    for row in df.itertuples(index=False):
+        del d[row["visit_id"]]
