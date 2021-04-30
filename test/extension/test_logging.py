a8a1dd9f6c06e4b41973eee79fd7493afba781c7
vringar <szabka@mozilla.com>
2021-04-30 12:50:33 +0200

    Extension logging restored and tested

--- test/extension/test_logging.py
+++ test/extension/test_logging.py
@@ -7,15 +7,16 @@ def test_extension_logging(task_manager_creator, default_params, tmp_path):
         browser_param.extra[
             "pre_instrumentation_code"
         ] = f"""
-            loggingDB.logWarn("{test_msg}");
+            // Weird name needed due to webpack name mangling
+            _loggingdb_js__WEBPACK_IMPORTED_MODULE_1__.logWarn("{test_msg}"); 
         """
 
     task_manager, _ = task_manager_creator((manager_params, browser_params))
     task_manager.close()
-    count = 0
+    log_list = []
     with open(log_path, "r") as log_file:
         for log_line in log_file:
             if test_msg in log_line:
-                count += 1
+                log_list.append(log_line)
     # We expect to see it once when printing the config and once when printing the log message
-    assert count == 2 * manager_params.num_browsers
+    assert len(log_line) == 2 * manager_params.num_browsers
