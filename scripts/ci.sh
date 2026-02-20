2a27b0e09e64b0963b4a4c1c96b6ca074a56807b
vringar <git@zabka.it>
2026-02-20 23:10:27 +0000

    ci: replace codecov package with codecov-action and add actionlint

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
 python -m pytest --cov=openwpm --junit-xml=junit-report.xml --cov-report=xml $TESTS -s -v --durations=10;
-exit_code=$?;
-if [[ "$exit_code" -ne 0 ]]; then
-    exit $exit_code;
-fi
-codecov -f coverage.xml;
-
