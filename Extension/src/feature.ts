8cf50d5b0d528aedce34c3a0da1b3ef9e391084a
vringar <git@zabka.it>
2026-02-27 00:06:33 +0000

    feat(stealth): add stealth JS instrumentation extension

--- Extension/src/feature.ts
+++ Extension/src/feature.ts
@@ -21,6 +21,7 @@ async function main() {
       navigation_instrument: true,
       cookie_instrument: true,
       js_instrument: true,
+      stealth_js_instrument: false,
       cleaned_js_instrument_settings: [
         {
           object: `window.CanvasRenderingContext2D.prototype`,
@@ -73,9 +74,16 @@ async function main() {
     cookieInstrument.run(config.browser_id);
   }
 
+  if (config.stealth_js_instrument) {
+    loggingDB.logDebug("Stealth JavaScript Instrumentation enabled");
+    const stealthJSInstrument = new JavascriptInstrument(loggingDB, false);
+    stealthJSInstrument.run(config.browser_id);
+    await stealthJSInstrument.registerContentScript(config.testing);
+  }
+
   if (config.js_instrument) {
     loggingDB.logDebug("Javascript instrumentation enabled");
-    const jsInstrument = new JavascriptInstrument(loggingDB);
+    const jsInstrument = new JavascriptInstrument(loggingDB, true);
     jsInstrument.run(config.browser_id);
     await jsInstrument.registerContentScript(
       config.testing,
